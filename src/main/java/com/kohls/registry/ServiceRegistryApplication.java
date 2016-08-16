f3fddb9d0984168ce18bbf510c1a8950af74bac4
sackot007 <sachin.kotekal@gmail.com>
2016-08-16 14:51:49 -0500

    First commit

---
+++ src/main/java/com/kohls/registry/ServiceRegistryApplication.java
@@ -0,0 +1,44 @@
+package com.kohls.registry;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		Connector ajpConnector = new Connector("AJP/1.3");
+		ajpConnector.setProtocol("AJP/1.3");
+		ajpConnector.setPort(8083);
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("http");
+		tomcat.addAdditionalTomcatConnectors(ajpConnector);
+		return tomcat;
+	}
+}
+
+@Controller
+class RedirectController {
+
+	@RequestMapping(value="/service-registry", method = RequestMethod.GET)
+	public String redirect() {
+		return "redirect:/";
+	}
+}
